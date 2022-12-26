588c7bc15571ff08b92d98b27a76a8b263aca0dc
Mimmi Sandgren <mimmisandgren@gmail.com>
2022-12-26 13:13:47 +0100

    init commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <p>YOU'RE CODE HAS BEEN ACTIVATED!</p>
+      
+      <div className="cannon">
+        <div class="confetti"></div>
+        <div class="confetti"></div>
+        <div class="confetti"></div>
+        <div class="confetti"></div>
+        <div class="confetti"></div>
+        <div class="confetti"></div>
+        <div class="confetti"></div>
+        <div class="confetti"></div>
+        <div class="confetti"></div>
+        <div class="confetti"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
