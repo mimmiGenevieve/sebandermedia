dc8fd4aee38d7378184ca2530e2d092302e379e4
mimmiGenevieve <mimmisandgren@gmail.com>
2023-01-04 18:30:13 +0100

    update to placeholder site

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import './App.css';
+import './App.css'
 
 function App() {
-  return (
-    <div className="app">
-      <p>YOU'RE CODE HAS BEEN ACTIVATED!</p>
-      
-      <div className="cannon">
-        <div class="confetti"></div>
-        <div class="confetti"></div>
-        <div class="confetti"></div>
-        <div class="confetti"></div>
-        <div class="confetti"></div>
-        <div class="confetti"></div>
-        <div class="confetti"></div>
-        <div class="confetti"></div>
-        <div class="confetti"></div>
-        <div class="confetti"></div>
-      </div>
-    </div>
-  );
+    return (
+        <div className="app">
+            <h1>
+                <span className="bold">SEBANDER</span>
+                <span className="thin"> | MEDIA</span>
+            </h1>
+            <span>COMIN SOON</span>
+        </div>
+    )
 }
 
-export default App;
+export default App
